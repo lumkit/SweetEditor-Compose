3147175df620539efa4077aa3c2325da80c26878
MingYubmy <mingyubmy@qq.com>
2026-04-21 11:49:01 +0800

    fix: correct reversed scroll direction on JVM platform

--- editor-compose/src/jvmMain/kotlin/com/qiplat/compose/sweeteditor/PlatformFeature.jvm.kt
+++ editor-compose/src/jvmMain/kotlin/com/qiplat/compose/sweeteditor/PlatformFeature.jvm.kt
@@ -5,4 +5,4 @@ import androidx.compose.ui.geometry.Offset
 actual fun getPlatformType(): PlatformType = PlatformType.Desktop
 
 actual fun normalizePlatformMouseWheelScrollDelta(scrollDelta: Offset): Offset =
-    if (scrollDelta == Offset.Zero) Offset.Zero else scrollDelta * 40f
+    if (scrollDelta == Offset.Zero) Offset.Zero else scrollDelta * -40f
